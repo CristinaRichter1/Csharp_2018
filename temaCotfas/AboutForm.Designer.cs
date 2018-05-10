c54b622131da952c2b5a228e8f05c213e89d9f78
Kovacs Erik <kovaroberik@gmail.com>
2018-05-10 22:59:10 +0300

    finsihed the about section

--- temaCotfas/AboutForm.Designer.cs
+++ temaCotfas/AboutForm.Designer.cs
@@ -28,21 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 14);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(260, 160);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 188);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "AboutForm";
             this.ShowIcon = false;
-            this.Text = "AboutForm";
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.AboutForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- temaCotfas/AboutForm.cs
+++ temaCotfas/AboutForm.cs
@@ -15,6 +15,31 @@ namespace temaCsharp
         public AboutForm()
         {
             InitializeComponent();
+            // Pull the About text from resource file -- it shoud contain a hyperlink,
+            // therefore attach appropriate event handler as well
+            String text = AboutResource.About;
+            richTextBox1.Text = text;
+            richTextBox1.LinkClicked += new LinkClickedEventHandler(richTextBox1_Clicked);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_Clicked(object sender, System.Windows.Forms.LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
         }
     }
 }
